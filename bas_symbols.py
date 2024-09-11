40087c3660c918541da84c8af94b4dc4b8a18caf
tutacat <fhwbml@gmail.com>
2024-09-11 22:02:13 +0100

    Start writing basic symbols

---
+++ bas_symbols.py
@@ -0,0 +1,35 @@
+"""print "hello, world"
+   print string(2 >= 3)
+   end
+"""
+
+symbols = {
+ "PRINT":r"print",
+ "INPUT":r"input",
+ "END":r"end",
+ "QUOTE":r'"',
+ "NUMBER":r"[0123456789]*\.[0123456789]+",
+ "FOR":r"for",
+ "FOR_TO":r"to",
+ "LBRACK":r'(',
+ "RBRACK":r')',
+ "EQUAL":r'==',
+ "STORE":r'=',
+ "INCREM":r'++',
+ "ADD":r'+',
+ "DECREM":r'--',
+ "SUBTRACT":r'-',
+ "DIVIDE":r'/',
+ "MULTIPLY":r'\*',
+ "":r'',
+ "":r'',
+ "":r'',
+ "":r'',
+ "":r'',
+ "":r'',
+ "":r'',
+ "":r'',
+}
+
+groups = {
+ "line": ""
